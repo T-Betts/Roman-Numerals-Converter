75ba33ecf5c88603f240b51c2794d9d0375d8a7a
Tom Betts <tom.betts@live.co.uk>
2019-02-04 16:31:54 +0000

    Update intToRomanNumerals to use Roman numerals object to map integers to Roman numerals

--- specs/int_to_roman_numerals_specs.js
+++ specs/int_to_roman_numerals_specs.js
@@ -5,4 +5,12 @@ describe('intToRomanNumerals', () => {
   it('should be able to convert 1 to I', () => {
     expect(intToRomanNumerals(1)).to.deep.equal('I');
   });
+
+  it('should be able to convert 5 to V', () => {
+    expect(intToRomanNumerals(5)).to.deep.equal('V');
+  });
+
+  it('should be able to convert 10 to X', () => {
+    expect(intToRomanNumerals(10)).to.deep.equal('X');
+  });
 });
--- src/int_to_roman_numerals.js
+++ src/int_to_roman_numerals.js
@@ -1,5 +1,12 @@
 function intToRomanNumerals(num) {
-  return "I";
+  const romanNumeralsMap = {
+    X: 10, V: 5, I: 1
+  };
+  result = '';
+  Object.keys(romanNumeralsMap).forEach((key) => {
+    if (romanNumeralsMap[key] === num) result += key;
+  });
+  return result;
 }
 
 module.exports.intToRomanNumerals = intToRomanNumerals;
