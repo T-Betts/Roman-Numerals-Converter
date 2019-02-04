9d1425fc38e6c0e29c8e87d9586e561f22ff7f39
Tom Betts <tom.betts@live.co.uk>
2019-02-04 17:12:04 +0000

    Update romanNumeralsToInt to use numeralValues object.

--- specs/roman_numerals_to_int_specs.js
+++ specs/roman_numerals_to_int_specs.js
@@ -5,4 +5,12 @@ describe('romanNumeralsToInt', () => {
   it('should be able to convert I to 1', () => {
     expect(romanNumeralsToInt('I')).to.deep.equal(1);
   });
+
+  it('should be able to convert V to 5', () => {
+    expect(romanNumeralsToInt('V')).to.deep.equal(5);
+  });
+
+  it('should be able to convert X to 10', () => {
+    expect(romanNumeralsToInt('X')).to.deep.equal(10);
+  });
 });
